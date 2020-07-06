f350e70943d34489ae315be949e9d7f538e7bdca
David Cortes <carvanasar@gmail.com>
2020-07-06 16:22:57 +0200

    Finalizado layout y estilos form crear-cuenta

--- pages/crear-cuenta.jsx
+++ pages/crear-cuenta.jsx
@@ -1,9 +1,45 @@
 import React from "react";
+import { css } from "@emotion/core";
 import Layout from "../componentes/layout/Layout";
+import { Formulario, Campo, InputSubmit } from "../componentes/ui/Formulario";
 const CrearCuenta = () => {
   return (
     <Layout>
-      <h1>Crear Cuenta</h1>
+      <>
+        <h1
+          css={css`
+            text-align: center;
+            margin-top: 5rem;
+          `}
+        >
+          Crear Cuenta
+        </h1>
+        <Formulario>
+          <Campo>
+            <label htmlFor="nombre">Nombre</label>
+            <input
+              type="text"
+              id="nombre"
+              placeholder="Tu Nombre"
+              name="nombre"
+            />
+          </Campo>
+          <Campo>
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" placeholder="Tu Email" name="email" />
+          </Campo>
+          <Campo>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Tu Password"
+              name="password"
+            />
+          </Campo>
+          <InputSubmit type="submit" value="Crear Cuenta" />
+        </Formulario>
+      </>
     </Layout>
   );
 };
