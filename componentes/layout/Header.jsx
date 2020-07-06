d8c07103e37926146dc6c6b793ffa1458998e899
David Cortes <cortesnietodavid@gmail.com>
2020-07-06 13:13:22 +0200

    Estilos y layout del header

--- componentes/layout/Header.jsx
+++ componentes/layout/Header.jsx
@@ -2,25 +2,82 @@ import React from "react";
 import Buscar from "../ui/Buscar";
 import Navegacion from "./Navegacion";
 import Link from "next/link";
+import Boton from "../ui/Boton";
+
+import styled from "@emotion/styled";
+import { css } from "@emotion/core";
 const Header = () => {
+  const usuario = true;
   return (
-    <header>
-      <div>
+    <header
+      css={css`
+        border-bottom: 2px solid var(--gris3);
+        padding: 1rem 0;
+      `}
+    >
+      <ContenedorHeader>
         <div>
-          <p>P</p>
+          <Link href="/">
+            <a>
+              <Logo>P</Logo>
+            </a>
+          </Link>
           <Buscar />
 
           <Navegacion />
         </div>
-        <div>
-          <p>Hola: David</p>
-          <button type="button">Cerrar Sesión</button>
-          <Link href="/">Login</Link>
-          <Link href="/">Crear Cuenta</Link>
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+          `}
+        >
+          {usuario ? (
+            <>
+              <p
+                css={css`
+                  margin-right: 2rem;
+                `}
+              >
+                Hola: David
+              </p>
+              <Boton bgColor="true">Cerrar Sesión</Boton>
+            </>
+          ) : (
+            <>
+              <Link href="/">
+                <Boton bgColor="true">Login</Boton>
+              </Link>
+              <Link href="/">
+                <Boton>Crear Cuenta</Boton>
+              </Link>
+            </>
+          )}
         </div>
-      </div>
+      </ContenedorHeader>
     </header>
   );
 };
 
 export default Header;
+
+// Styled Components
+
+const ContenedorHeader = styled.div`
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+const Logo = styled.p`
+  color: var(--naranja);
+  font-size: 4rem;
+  line-height: 0;
+  font-weight: 700;
+  font-family: "Roboto Slab", serif;
+  margin-right: 2rem;
+`;
