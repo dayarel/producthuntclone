82ef94205eb785683ead650546c527e25b60388b
David Cortes <carvanasar@gmail.com>
2020-07-08 17:48:57 +0200

    Creado setup para firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyApGTO83XJ4-k70zx8BXj5kn7e_QhKNtx0",
+  authDomain: "product-hunt-1e4aa.firebaseapp.com",
+  databaseURL: "https://product-hunt-1e4aa.firebaseio.com",
+  projectId: "product-hunt-1e4aa",
+  storageBucket: "product-hunt-1e4aa.appspot.com",
+  messagingSenderId: "1084077829924",
+  appId: "1:1084077829924:web:08f630cf9a1b44b6930db1",
+};
+
+export default firebaseConfig;
